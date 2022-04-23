2f79268998d5fabb178e0b1b81625dca6eaf7bc6
Rabindra <drarabin@gmail.com>
2022-04-23 18:04:58 +0545

    factories

---
+++ factories.js
@@ -0,0 +1,14 @@
+// Factory Function
+function createCircle(radius) {
+  return {
+    // radius: radius,
+    radius, //same as above
+    draw: function () {
+      console.log("draw");
+    },
+  };
+}
+
+const circle = createCircle(1);
+
+circle.draw();
