3631f1c4c1081f91e36840d951d6546f6f42ee87
Rabindra <drarabin@gmail.com>
2022-04-23 17:46:05 +0545

    object literals

---
+++ object_literals.js
@@ -0,0 +1,14 @@
+// object in JS is key-value pair
+
+const circle = {
+  radius: 1,
+  location: {
+    x: 1,
+    y: 1,
+  },
+  draw: function () {
+    console.log("draw circle");
+  },
+};
+
+circle.draw();
